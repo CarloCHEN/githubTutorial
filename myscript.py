85283700a0e411e07544948706d23cd24e81920e
CarloCHEN <jiaxuchen16@gmail.com>
2020-02-10 11:21:34 -0800

    new code

--- myscript.py
+++ myscript.py
@@ -1,10 +1,11 @@
 import numpy as np
-my_matrix = np.identity(4)
-print(my_matrix)
-my_matrix2 = np.array(range(100))
-print(my_matrix2)
-def helloworld():
-  print('Hello World')
 
-if '__name__' == __main__:
-  helloworld()
+def Myfunction(my_matrix, my_matrix2):
+    return my_matrix * my_matrix2
+
+my_matrix = np.identity(4)
+
+my_matrix2 = np.array(range(100))
+
+
+print(Myfunction(my_matrix, my_matrix2))
