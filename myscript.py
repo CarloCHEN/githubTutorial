cb2489d8940ce4ddb72a4ddacaa320560d4f40ac
CarloCHEN <jiaxuchen16@gmail.com>
2020-02-10 11:26:00 -0800

    a function has been updated

--- myscript.py
+++ myscript.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 def Myfunction(my_matrix, my_matrix2):
-    return my_matrix * my_matrix2
+    try:
+        return my_matrix * my_matrix2
+    except ValueError:
+        return None
 
 my_matrix = np.identity(4)
 
-my_matrix2 = np.identity(4)
+my_matrix2 = np.random.random(size=(4,5))
 
 
 print(Myfunction(my_matrix, my_matrix2))
