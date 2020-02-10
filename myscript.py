b9990bb4a72f42e86a33b7b660ff21751a977c11
CarloCHEN <jiaxuchen16@gmail.com>
2020-02-10 11:01:04 -0800

    I added a new function

--- myscript.py
+++ myscript.py
@@ -1,5 +1,10 @@
 import numpy as np
 my_matrix = np.identity(4)
 print(my_matrix)
-my_matrix2 = list(range(100))
+my_matrix2 = np.array(range(100))
 print(my_matrix2)
+def helloworld():
+  print('Hello World')
+
+if '__name__' == __main__:
+  helloworld()
